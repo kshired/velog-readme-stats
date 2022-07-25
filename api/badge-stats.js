ee9e10343dd1c3d141a18c0691ebf40a4e67e135
kshired <shiroed1211@naver.com>
2022-07-26 05:06:28 +0900

    feat: api/badge-stats-dark

--- api/badge-stats.js
+++ api/badge-stats.js
@@ -1,12 +1,13 @@
 const createBadge = require("../src/cards/velog-badge-with-stats");
+const createBadgeDark = require("../src/cards/velog-badge-with-stats-black");
 const getStats = require("../src/fetchers/stats-fetcher");
 
 module.exports = async (req, res) => {
-  const { name } = req.query;
+  const { name, color } = req.query;
   res.setHeader("Content-Type", "image/svg+xml");
   try {
     const {totalLikes, totalViews} = await getStats(name);
-    return res.send(createBadge(name, totalLikes, totalViews));
+    return res.send(color === "dark" ? createBadgeDark(name, totalLikes, totalViews) : createBadge(name, totalLikes, totalViews));
   } catch (e) {
     return res.send(e.message);
   }
